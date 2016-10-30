567dc9d9057c42e4cc1455812ac5df9a16616b27
BojanV03-W10 <bvasiljevic95@gmail.com>
2016-10-30 12:40:30 +0100

    Generated Visual Studio C++ project files

---
+++ RG_Project/Source/RG_Project/RG_Project.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RG_Project.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RG_Project, "RG_Project" );
